edc52e80d781e261c526f3ba6be713dd3857baf5
lakha <lakha@statoil-net.tfs.local>
2012-01-02 15:22:11 +0000

    git-tfs-id: [http://tfs.statoil.net:8080/tfs/TFS2005]$/Statoil.SmaJobbSentralen.RetailSE/Source/NoiseCalculator/Source/Main;C18086

--- NoiseCalculator.UI.Web/Controllers/HomeController.cs
+++ NoiseCalculator.UI.Web/Controllers/HomeController.cs
@@ -1,11 +1,17 @@
 ﻿using System.Collections.Generic;
 using System.Web.Mvc;
+using System.Web.Routing;
 using NoiseCalculator.Domain.Entities;
 using NoiseCalculator.Infrastructure.DataAccess.Interfaces;
-using NoiseCalculator.Infrastructure.NHibernate;
+
 
 namespace NoiseCalculator.Controllers
 {
+    public class TaskFormRequestModel
+    {
+        public int selectedTask { get; set; }
+    }
+
     public class HomeController : Controller
     {
         private readonly IDAO<Task, int> _taskDAO;
@@ -25,7 +31,49 @@ namespace NoiseCalculator.Controllers
         public PartialViewResult AddTask()
         {
             IEnumerable<Task> tasks = _taskDAO.GetAll();
-            return PartialView("_TaskForm", tasks);
+            return PartialView("_TaskFormCommon", tasks);
+        }
+
+        [HttpPost]
+        public PartialViewResult AddTask(TaskFormRequestModel taskFormRequestModel)
+        {
+            Task task = _taskDAO.Get(taskFormRequestModel.selectedTask);
+
+            switch (task.Role.Title)
+            {
+                case "Helideck":
+                    {
+                        return AddTaskHelideck(task);
+                        break;
+                    }
+                case "Rotation":
+                    {
+                        return AddTaskRotation(task);
+                        break;
+                    }
+                default:
+                    {
+                        return AddTaskRegular(task);
+                        break;
+                    }
+            }
+        }
+        
+        
+        // ------------------------------------------------
+        public PartialViewResult AddTaskRegular(Task task)
+        {
+            return PartialView("_TaskFormRegular");
+        }
+
+        public PartialViewResult AddTaskHelideck(Task task)
+        {
+            return PartialView("_TaskFormHelideck", task);
+        }
+
+        public PartialViewResult AddTaskRotation(Task task)
+        {
+            return PartialView("_TaskFormRotation");
         }
 
         public ActionResult About()
