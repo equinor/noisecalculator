fdab7f271baa1aca98dd34c3736f37fb7656e5f9
lakha <lakha@statoil-net.tfs.local>
2012-01-11 13:07:45 +0000

    git-tfs-id: [http://tfs.statoil.net:8080/tfs/TFS2005]$/Statoil.SmaJobbSentralen.RetailSE/Source/NoiseCalculator/Source/Main;C18112

--- NoiseCalculator.Domain/Entities/SelectedTask.cs
+++ NoiseCalculator.Domain/Entities/SelectedTask.cs
@@ -2,16 +2,16 @@
 {
     public class SelectedTask
     {
-        public int Id { get; private set; }
-        public string Title { get; set; }
-        public string Role { get; set; }
-        public string NoiseProtection { get; set; }
-        public int NoiseLevel { get; set; }
-        public int Hours { get; set; }
-        public int Minutes { get; set; }
-        public int Percentage { get; set; }
+        public virtual int Id { get; private set; }
+        public virtual string Title { get; set; }
+        public virtual string Role { get; set; }
+        public virtual string NoiseProtection { get; set; }
+        public virtual int NoiseLevel { get; set; }
+        public virtual int Hours { get; set; }
+        public virtual int Minutes { get; set; }
+        public virtual int Percentage { get; set; }
 
-        public int TaskId { get; set; }
-        public int HelicopterTaskId { get; set; }
+        public virtual int TaskId { get; set; }
+        public virtual int HelicopterTaskId { get; set; }
     }
 }
