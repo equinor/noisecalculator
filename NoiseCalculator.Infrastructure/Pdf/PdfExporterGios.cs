82c644a14d5a9c7e6359ae6b6fa8763e537ee70e
lakha <lakha@statoil-net.tfs.local>
2012-04-27 10:20:09 +0000

    git-tfs-id: [http://tfs.statoil.net:8080/tfs/TFS2005]$/Statoil.SmaJobbSentralen.RetailSE/Source/NoiseCalculator/Source/Main;C19313

--- NoiseCalculator.Infrastructure/Pdf/PdfExporterGios.cs
+++ NoiseCalculator.Infrastructure/Pdf/PdfExporterGios.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Data;
 using System.Drawing;
 using System.IO;
@@ -143,7 +142,7 @@ namespace NoiseCalculator.Infrastructure.Pdf
             Stream memoryStream = new MemoryStream();
             myPdfDocument.SaveToStream(memoryStream);
             memoryStream.Seek(0, SeekOrigin.Begin);
-            
+
             return memoryStream;
         }
 
