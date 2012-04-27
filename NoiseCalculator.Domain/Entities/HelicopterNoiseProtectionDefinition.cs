72fd7881e9406f5f7a93c6c8e80c71c692c77ae8
lakha <lakha@statoil-net.tfs.local>
2012-04-27 16:28:46 +0000

    git-tfs-id: [http://tfs.statoil.net:8080/tfs/TFS2005]$/Statoil.SmaJobbSentralen.RetailSE/Source/NoiseCalculator/Source/Main;C19332

--- NoiseCalculator.Domain/Entities/HelicopterNoiseProtectionDefinition.cs
+++ NoiseCalculator.Domain/Entities/HelicopterNoiseProtectionDefinition.cs
@@ -1,8 +1,11 @@
+using System.Collections.Generic;
+
 namespace NoiseCalculator.Domain.Entities
 {
     public class HelicopterNoiseProtectionDefinition
     {
         public virtual int Id { get; set; }
         public virtual string SystemName { get; set; }
+        public virtual IList<HelicopterNoiseProtection> HelicopterNoiseProtections { get; set; }
     }
 }
