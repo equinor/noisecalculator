d31f03088ebe43c793e6622c3ef1b04c863bbdc9
lakha <lakha@statoil-net.tfs.local>
2012-05-03 13:59:35 +0000

    git-tfs-id: [http://tfs.statoil.net:8080/tfs/TFS2005]$/Statoil.SmaJobbSentralen.RetailSE/Source/NoiseCalculator/Source/Main;C19352

---
+++ NoiseCalculator.Infrastructure/Mapping/HelicopterTaskDefinitionMap.cs
@@ -0,0 +1,15 @@
+﻿using FluentNHibernate.Mapping;
+using NoiseCalculator.Domain.Entities;
+
+namespace NoiseCalculator.Infrastructure.Mapping
+{
+    public class HelicopterTaskDefinitionMap : ClassMap<HelicopterTaskDefinition>
+    {
+        public HelicopterTaskDefinitionMap()
+        {
+            Id(x => x.Id);
+
+            Map(x => x.Title);
+        }
+    }
+}
