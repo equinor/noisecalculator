f698c7cac57a162dcdaca34e46615f4e228152cb
lakha <lakha@statoil-net.tfs.local>
2012-05-03 13:59:57 +0000

    git-tfs-id: [http://tfs.statoil.net:8080/tfs/TFS2005]$/Statoil.SmaJobbSentralen.RetailSE/Source/NoiseCalculator/Source/Main;C19353

--- NoiseCalculator.Infrastructure/DataAccess/Implementations/HelicopterTaskDAO.cs
+++ NoiseCalculator.Infrastructure/DataAccess/Implementations/HelicopterTaskDAO.cs
@@ -12,12 +12,24 @@ namespace NoiseCalculator.Infrastructure.DataAccess.Implementations
         {
         }
 
-        public HelicopterTask Get(int helicopterId, HelicopterNoiseProtectionDefinition noiseProtectionDefinition, int workIntervalId)
+        //public HelicopterTask Get(int helicopterId, HelicopterNoiseProtectionDefinition noiseProtectionDefinition, int workIntervalId)
+        //{
+        //    HelicopterTask helicopterTask = _session.QueryOver<HelicopterTask>()
+        //        .Where(x => x.HelicopterType.Id == helicopterId)
+        //        .And(x => x.HelicopterWorkInterval.Id == workIntervalId)
+        //        .And(x => x.HelicopterNoiseProtectionDefinition == noiseProtectionDefinition)
+        //        .Fetch(x => x.HelicopterType).Eager
+        //        .Fetch(x => x.HelicopterWorkInterval).Eager
+        //        .SingleOrDefault<HelicopterTask>();
+
+        //    return helicopterTask;
+        //}
+        public HelicopterTask Get(int helicopterId, int noiseProtectionId, int workIntervalId)
         {
             HelicopterTask helicopterTask = _session.QueryOver<HelicopterTask>()
                 .Where(x => x.HelicopterType.Id == helicopterId)
                 .And(x => x.HelicopterWorkInterval.Id == workIntervalId)
-                .And(x => x.HelicopterNoiseProtectionDefinition == noiseProtectionDefinition)
+                .And(x => x.HelicopterNoiseProtection.Id == noiseProtectionId)
                 .Fetch(x => x.HelicopterType).Eager
                 .Fetch(x => x.HelicopterWorkInterval).Eager
                 .SingleOrDefault<HelicopterTask>();
