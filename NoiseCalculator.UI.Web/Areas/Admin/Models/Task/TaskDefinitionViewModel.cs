16139fd9730a4d63c3a3973fa5972345b0b9e3f1
lakha <lakha@statoil-net.tfs.local>
2012-05-10 19:16:47 +0000

    git-tfs-id: [http://tfs.statoil.net:8080/tfs/TFS2005]$/Statoil.SmaJobbSentralen.RetailSE/Source/NoiseCalculator/Source/Main;C19383

---
+++ NoiseCalculator.UI.Web/Areas/Admin/Models/Task/TaskDefinitionViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace NoiseCalculator.UI.Web.Areas.Admin.Models.Task
+{
+    public class TaskDefinitionViewModel
+    {
+        public int Id { get; set; }
+        public string SystemName { get; set; }
+
+        public IList<TaskListItemViewModel> Tasks { get; set; }
+
+        public string UrlCreateTranslation { get; set; }
+        public string UrlEditTranslation { get; set; }
+        public string UrlDeleteTranslationConfirmation { get; set; }
+    }
+}
---
+++ NoiseCalculator.UI.Web/Areas/Admin/Models/Task/TaskListItemViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace NoiseCalculator.UI.Web.Areas.Admin.Models.Task
+{
+    public class TaskListItemViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int NoiseLevelGuideline { get; set; }
+        public int AllowedExposureMinutes { get; set; }
+        public string Language { get; set; }
+    }
+}
