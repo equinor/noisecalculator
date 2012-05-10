c2209178cf6a8fb2e8a13e536fc61635f8b97c15
lakha <lakha@statoil-net.tfs.local>
2012-05-10 19:30:12 +0000

    git-tfs-id: [http://tfs.statoil.net:8080/tfs/TFS2005]$/Statoil.SmaJobbSentralen.RetailSE/Source/NoiseCalculator/Source/Main;C19385

--- NoiseCalculator.Domain/Entities/HelicopterTaskDefinition.cs
+++
@@ -1,8 +0,0 @@
-namespace NoiseCalculator.Domain.Entities
-{
-    public class HelicopterTaskDefinition
-    {
-        public virtual int Id { get; set; }
-        public virtual string Title { get; set; }
-    }
-}
