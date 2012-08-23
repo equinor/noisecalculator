1ea243f09f9cd4ef554991618a3f235fccbe1d80
lakha <lakha@statoil-net.tfs.local>
2012-08-23 14:05:32 +0000

    Updated Helicopter Task Translation.
    git-tfs-id: [http://tfs.statoil.net:8080/tfs/TFS2005]$/Statoil.SmaJobbSentralen.RetailSE/Source/NoiseCalculator/Source/Main;C19987

--- NoiseCalculator.UI.Web/Areas/Admin/Resources/AdminResources.Designer.cs
+++ NoiseCalculator.UI.Web/Areas/Admin/Resources/AdminResources.Designer.cs
@@ -295,7 +295,7 @@ namespace NoiseCalculator.UI.Web.Areas.Admin.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Noise Protection Definition.
+        ///   Looks up a localized string similar to Noise Protection.
         /// </summary>
         public static string HelicopterTaskColumnNoiseProtectionDefinition {
             get {
