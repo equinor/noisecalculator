9bb6888c9e9ae0c29b06c9a1b34546535b284d7f
lakha <lakha@statoil-net.tfs.local>
2012-08-24 13:25:03 +0000

    Added footnotes service. The logic and resource are moved from UI layer.
    git-tfs-id: [http://tfs.statoil.net:8080/tfs/TFS2005]$/Statoil.SmaJobbSentralen.RetailSE/Source/NoiseCalculator/Source/Main;C20007

---
+++ NoiseCalculator.Domain/DomainServices/FootnotesService.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NoiseCalculator.Domain.Entities;
+using NoiseCalculator.Domain.Enums;
+using NoiseCalculator.Domain.Resources;
+
+namespace NoiseCalculator.Domain.DomainServices
+{
+    public class FootnotesService : IFootnotesService
+    {
+        public IEnumerable<string> CalculateFootnotes(IEnumerable<SelectedTask> selectedTasks)
+        {
+            List<string> footnotes = GetStaticFootnotes();
+            footnotes.AddRange(CalculateDynamicFootnotes(selectedTasks));
+            
+            return footnotes;
+        }
+        
+        private List<string> GetStaticFootnotes()
+        {
+            List<string> staticFootnotes = new List<string>
+                {
+                    DomainResources.FooterGL0169,
+                    DomainResources.FooterNoiseProtectionDefinition,
+                    DomainResources.FooterUseNoisyWorkWhenTaskIsMissing,
+                    DomainResources.Footer80dBA
+                };
+
+            return staticFootnotes;
+        }
+
+        private IEnumerable<string> CalculateDynamicFootnotes(IEnumerable<SelectedTask> selectedTasks)
+        {
+            bool hasNoisyWork = (selectedTasks.Any(x => x.Task.Role.RoleType != RoleTypeEnum.AreaNoise));
+            bool hasRegularTasks = (hasNoisyWork && selectedTasks.Any(x => x.HelicopterTaskId == 0));
+
+            IList<string> dynamicFootnotes = new List<string>();
+
+            if (hasNoisyWork)
+            {
+                dynamicFootnotes.Add(DomainResources.FooterDynamicNoiseProtection);
+                dynamicFootnotes.Add(DomainResources.FooterDynamicCorrectionForMeasuredNoiseLevel);
+
+                if (hasRegularTasks)
+                {
+                    dynamicFootnotes.Add(DomainResources.FooterDynamicValidForAreaNoiseUpTo90dBA);
+                }
+            }
+
+            return dynamicFootnotes;
+        }
+    }
+}
---
+++ NoiseCalculator.Domain/DomainServices/IFootnotesService.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using NoiseCalculator.Domain.Entities;
+
+namespace NoiseCalculator.Domain.DomainServices
+{
+    public interface IFootnotesService
+    {
+        IEnumerable<string> CalculateFootnotes(IEnumerable<SelectedTask> selectedTasks);
+    }
+}
