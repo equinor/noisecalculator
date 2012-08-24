a327846c83c6e09d62c9074836607de18c76d23f
lakha <lakha@statoil-net.tfs.local>
2012-08-24 13:30:37 +0000

    Responsibility cleanup. Moved code for getting correct create and edit URLs to the UI (JS) itself. Moved PDFReport generation to separate controller.
    git-tfs-id: [http://tfs.statoil.net:8080/tfs/TFS2005]$/Statoil.SmaJobbSentralen.RetailSE/Source/NoiseCalculator/Source/Main;C20014

--- NoiseCalculator.UI.Web/Controllers/TaskController.cs
+++ NoiseCalculator.UI.Web/Controllers/TaskController.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,9 +7,7 @@ using NoiseCalculator.Domain.DomainServices;
 using NoiseCalculator.Domain.Entities;
 using NoiseCalculator.Domain.Enums;
 using NoiseCalculator.Infrastructure.DataAccess.Interfaces;
-using NoiseCalculator.Infrastructure.Pdf;
 using NoiseCalculator.UI.Web.Models;
-using NoiseCalculator.UI.Web.Resources;
 using NoiseCalculator.UI.Web.ViewModels;
 
 namespace NoiseCalculator.UI.Web.Controllers
@@ -19,19 +16,17 @@ namespace NoiseCalculator.UI.Web.Controllers
     {
         private readonly ITaskDAO _taskDAO;
         private readonly ISelectedTaskDAO _selectedTaskDAO;
-        private readonly IPdfExporter _pdfExporter;
         private readonly INoiseLevelService _noiseLevelService;
-        private readonly IRoleDAO _roleDAO;
         private readonly IAdministratorDAO _administratorDAO;
+        private readonly IFootnotesService _footnotesService;
 
-        public TaskController(ITaskDAO taskDAO, ISelectedTaskDAO selectedTaskDAO, IPdfExporter pdfExporter, INoiseLevelService noiseLevelService, IRoleDAO roleDAO, IAdministratorDAO administratorDAO)
+        public TaskController(ITaskDAO taskDAO, ISelectedTaskDAO selectedTaskDAO, INoiseLevelService noiseLevelService, IAdministratorDAO administratorDAO, IFootnotesService footnotesService)
         {
             _taskDAO = taskDAO;
             _selectedTaskDAO = selectedTaskDAO;
-            _pdfExporter = pdfExporter;
             _noiseLevelService = noiseLevelService;
-            _roleDAO = roleDAO;
             _administratorDAO = administratorDAO;
+            _footnotesService = footnotesService;
         }
         
 
@@ -51,63 +46,15 @@ namespace NoiseCalculator.UI.Web.Controllers
         }
 
 
-        public ActionResult PdfReport(ReportInfo reportInfo)
-        {
-            reportInfo.CreatedBy = UserHelper.CreateUsernameWithoutDomain(User.Identity.Name);
-            IEnumerable<SelectedTask> selectedTasks = _selectedTaskDAO.GetAllChronologically(User.Identity.Name, DateTime.Now);
-
-            if (selectedTasks.Count() > 0)
-            {
-                reportInfo.Footnotes.AddRange(FootnoteResources.GetStaticFootnotes());
-                reportInfo.Footnotes.AddRange(GetDynamicFootnotes(selectedTasks));
-
-                Stream memoryStream = _pdfExporter.GenerateSelectedTasksPDF(selectedTasks, reportInfo);
-                HttpContext.Response.AddHeader("content-disposition", "attachment; filename=MyTasks-" + DateTime.Now.Date.ToShortDateString() + ".pdf");
-                return new FileStreamResult(memoryStream, "application/pdf");
-            }
-
-            // No tasks found - Redirect to main page
-            return RedirectToAction("Index");
-        }
-
-
         public PartialViewResult AddTask()
         {
-            IEnumerable<Task> tasks = _taskDAO.GetAllOrdered();
-
+            IEnumerable<TaskSelectViewModel> tasks = _taskDAO.GetAllOrdered().Select(task => new TaskSelectViewModel(task));
+            
             Response.Cache.SetCacheability(HttpCacheability.NoCache);
 
             return PartialView("_TaskDialog", tasks);
         }
 
-
-        public ActionResult GetCreateFormForTask(int id)
-        {
-            Task task = _taskDAO.GetFilteredByCurrentCulture(id);
-            
-            switch (task.Role.RoleType)
-            {
-                case RoleTypeEnum.Helideck:
-                    return RedirectToAction("AddTaskHelideck", "Helideck", new { TaskId = task.Id });
-                case RoleTypeEnum.Rotation:
-                    return RedirectToAction("AddTaskRotation", "Rotation", new { TaskId = task.Id });
-                default:
-                    return RedirectToAction("AddTaskRegular", "Regular", new { TaskId = task.Id });
-            }
-        }
-
-        public ActionResult GetEditFormForSelectedTask(int id)
-        {
-            SelectedTask selectedTask = _selectedTaskDAO.Get(id);
-            
-            switch (selectedTask.Task.Role.Title)
-            {
-                case "Helideck":
-                    return RedirectToAction("EditTaskHelideck", "Helideck", new { selectedTaskId = selectedTask.Id });
-                default:
-                    return RedirectToAction("EditTaskRegular", "Regular", new { selectedTaskId = selectedTask.Id });
-            }
-        }
 
         public ActionResult GetRemoveTaskConfirmationDialog(int id)
         {
@@ -187,44 +134,9 @@ namespace NoiseCalculator.UI.Web.Controllers
                     break;
             }
 
-            totalNoiseDosage.DynamicFootnotes = GetDynamicFootnotes(selectedTasks);
+            totalNoiseDosage.Footnotes = _footnotesService.CalculateFootnotes(selectedTasks);
 
             return Json(totalNoiseDosage, JsonRequestBehavior.AllowGet);
-        }
-
-        public IList<string> GetDynamicFootnotes(IEnumerable<SelectedTask> selectedTasks)
-        {
-            bool hasNoisyWork = false;
-            bool hasRegularTasks = false;
-            var roleIDs = _roleDAO.GetAreaNoiseRoleIds();
-            
-            foreach (SelectedTask selectedTask in selectedTasks)
-            {
-                if(roleIDs.Contains(selectedTask.Task.Role.Id) == false)
-                {
-                    hasNoisyWork = true;
-
-                    if (selectedTask.HelicopterTaskId == 0)
-                    {
-                        hasRegularTasks = true;
-                    }
-                }
-            }
-
-            IList<string> dynamicFootnotes = new List<string>();
-            
-            if(hasNoisyWork)
-            {
-                dynamicFootnotes.Add(TaskResources.FooterDynamicNoiseProtection);
-                dynamicFootnotes.Add(TaskResources.FooterDynamicCorrectionForMeasuredNoiseLevel);
-
-                if (hasRegularTasks)
-                {
-                    dynamicFootnotes.Add(TaskResources.FooterDynamicValidForAreaNoiseUpTo90dBA);
-                }
-            }
-
-            return dynamicFootnotes;
         }
     }
 }
