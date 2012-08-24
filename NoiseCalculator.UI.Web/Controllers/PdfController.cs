b1763d6257e4233968ca123d782838710dfc5b14
lakha <lakha@statoil-net.tfs.local>
2012-08-24 13:27:24 +0000

    Moved PDF report logic from TaskController.
    git-tfs-id: [http://tfs.statoil.net:8080/tfs/TFS2005]$/Statoil.SmaJobbSentralen.RetailSE/Source/NoiseCalculator/Source/Main;C20010

---
+++ NoiseCalculator.UI.Web/Controllers/PdfController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web.Mvc;
+using NoiseCalculator.Domain.DomainServices;
+using NoiseCalculator.Domain.Entities;
+using NoiseCalculator.Infrastructure.DataAccess.Interfaces;
+using NoiseCalculator.Infrastructure.Pdf;
+using NoiseCalculator.UI.Web.Models;
+
+namespace NoiseCalculator.UI.Web.Controllers
+{
+    public class PdfController : Controller
+    {
+        private readonly ISelectedTaskDAO _selectedTaskDAO;
+        private readonly IFootnotesService _footnotesService;
+        private readonly IPdfExporter _pdfExporter;
+
+        public PdfController(ISelectedTaskDAO selectedTaskDAO, IFootnotesService footnotesService, IPdfExporter pdfExporter)
+        {
+            _selectedTaskDAO = selectedTaskDAO;
+            _footnotesService = footnotesService;
+            _pdfExporter = pdfExporter;
+        }
+
+        public ActionResult PdfReportCurrentTasks(ReportInfo reportInfo)
+        {
+            reportInfo.CreatedBy = UserHelper.CreateUsernameWithoutDomain(User.Identity.Name);
+            IEnumerable<SelectedTask> selectedTasks = _selectedTaskDAO.GetAllChronologically(User.Identity.Name, DateTime.Now);
+
+            if (selectedTasks.Count() > 0)
+            {
+                reportInfo.Footnotes = _footnotesService.CalculateFootnotes(selectedTasks);
+
+                Stream memoryStream = _pdfExporter.GenerateSelectedTasksPDF(selectedTasks, reportInfo);
+                HttpContext.Response.AddHeader("content-disposition", "attachment; filename=MyTasks-" + DateTime.Now.Date.ToShortDateString() + ".pdf");
+                return new FileStreamResult(memoryStream, "application/pdf");
+            }
+
+            // No tasks found - Redirect to main page
+            return RedirectToAction("Index", "Task");
+        }
+
+    }
+}
