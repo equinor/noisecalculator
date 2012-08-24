5659df9a181c85c5022d34733932c12ccdcb72d3
lakha <lakha@statoil-net.tfs.local>
2012-08-24 13:28:08 +0000

    Added to prevent using the Task entity directly and because I needed the RoleType enum property from Task.Role.
    git-tfs-id: [http://tfs.statoil.net:8080/tfs/TFS2005]$/Statoil.SmaJobbSentralen.RetailSE/Source/NoiseCalculator/Source/Main;C20011

---
+++ NoiseCalculator.UI.Web/ViewModels/TaskSelectViewModel.cs
@@ -0,0 +1,20 @@
+﻿using NoiseCalculator.Domain.Entities;
+
+namespace NoiseCalculator.UI.Web.ViewModels
+{
+    public class TaskSelectViewModel
+    {
+        public int Id { get; private set; }
+        public string Title { get; private set; }
+        public string Role { get; private set; }
+        public string RoleType { get; private set; }
+        
+        public TaskSelectViewModel(Task task)
+        {
+            Id = task.Id;
+            Title = task.Title;
+            Role = task.Role.Title;
+            RoleType = task.Role.RoleType.ToString();
+        }
+    }
+}
